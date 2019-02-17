9e34679566b8aa8eefb527dfbb7bc968c2c47b52
Carlos <Carlos_Gamino@baylor.edu>
2019-02-17 15:19:01 -0600

    testing

--- script.js
+++ script.js
@@ -60,8 +60,5 @@ $('.responsive').slick({
         slidesToScroll: 1
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
 });
