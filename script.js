4b259f88c830613bccad12aa45edbdff79ff905b
Carlos <Carlos_Gamino@baylor.edu>
2019-02-17 17:35:02 -0600

    format

--- script.js
+++ script.js
@@ -26,39 +26,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
-
-// Test
-$('.responsive').slick({
-  dots: true,
-	prevArrow: $('.prev'),
-	nextArrow: $('.next'),
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
